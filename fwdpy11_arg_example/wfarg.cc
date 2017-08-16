e839e4128249b2f93809c8f36962ad52ddde22a4
molpopgen <krthornt@uci.edu>
2017-08-16 14:48:47 -0700

    register types for buffer protocol/numpy access

--- fwdpy11_arg_example/wfarg.cc
+++ fwdpy11_arg_example/wfarg.cc
@@ -12,5 +12,23 @@ PYBIND11_PLUGIN(wfarg)
     py::module m("wfarg", "Simple example of Wright-Fisher simulation with "
                           "selection and ARG tracking");
 
-	return m.ptr();
+    //Register nodes and edges as NumPy dtypes:
+    PYBIND11_NUMPY_DTYPE(node, id, generation, deme);
+    PYBIND11_NUMPY_DTYPE(edge, left, right, parent, child);
+
+    //Register vectors of nodes and edges as "opaque"
+    //types supporting Python's buffer protocol, creating
+    //Python classes that are castable to NumPy structured
+    //arrays without a copy.  
+    py::bind_vector<std::vector<node>>(
+        m, "NodeArray", "Container of nodes. This can be cast to a NumPy "
+                        "record array without making a copy.",
+        py::buffer_protocol());
+
+    py::bind_vector<std::vector<edge>>(
+        m, "EdgeArray", "Container of edges.  This can be cast to a NumPy "
+                        "record array without making a copy",
+        py::buffer_protocol());
+
+    return m.ptr();
 }
