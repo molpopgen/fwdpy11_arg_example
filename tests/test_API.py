994d91401cbbbf5dee61ea5629a97cf326fd0efd
molpopgen <krthornt@uci.edu>
2017-08-17 14:08:09 -0700

    add test that ArgWrapper is callable

--- tests/test_API.py
+++ tests/test_API.py
@@ -1,5 +1,12 @@
 import unittest
 
+class tests_ArgWrapper(unittest.TestCase):
+    def test_callable(self):
+        from fwdpy11_arg_example.ArgWrapper import ArgWrapper
+        self.assertEqual(callable(ArgWrapper),True)
+        a = ArgWrapper(10)
+        self.assertEqual(callable(a),True)
+
 class testAPI(unittest.TestCase):
     def test_quick_sim(self):
         from fwdpy11_arg_example.evolve_arg import evolve_track_wrapper
