18feb11fbf0903ba0c1c106da9af308a9d7d192c
molpopgen <krthornt@uci.edu>
2017-08-21 14:18:09 -0700

    autopep8

--- practice/make_trees.py
+++ practice/make_trees.py
@@ -13,30 +13,30 @@ for s, t, p in zip(sv, tv, pv):
 print(n)
 
 n = msprime.NodeTable()
-n.set_columns(flags=sv,population=pv,time=tv)
+n.set_columns(flags=sv, population=pv, time=tv)
 
 print(n)
 
-left = [0.2,0.0,0.2,0.8,0.8,0.0]
-right = [0.8,0.2,0.8,1.0,1.0,0.2]
-parent = [3,4,4,4,5,6]
-#children = [(0,2),(1,2),(1,3),(0,4),(0,4)]
-children = [(0,2),(1,2),(1,3),(1,2),(0,4),(0,4)]
+left = [0.2, 0.0, 0.2, 0.8, 0.8, 0.0]
+right = [0.8, 0.2, 0.8, 1.0, 1.0, 0.2]
+parent = [3, 4, 4, 4, 5, 6]
+# children = [(0,2),(1,2),(1,3),(0,4),(0,4)]
+children = [(0, 2), (1, 2), (1, 3), (1, 2), (0, 4), (0, 4)]
 
 e = msprime.EdgesetTable()
 
-#e.set_columns(left=left,right=right,parent=parent,children=children,children_length=len(children))
-for l,r,p,c in zip(left,right,parent,children):
-    e.add_row(left=l,right=r,parent=p,children=c)
+# e.set_columns(left=left,right=right,parent=parent,children=children,children_length=len(children))
+for l, r, p, c in zip(left, right, parent, children):
+    e.add_row(left=l, right=r, parent=p, children=c)
 
 
 print(e)
 
-msprime.sort_tables(nodes=n,edgesets=e)
+msprime.sort_tables(nodes=n, edgesets=e)
 
 print(n)
 
-ts = msprime.load_tables(nodes=n,edgesets=e)
+ts = msprime.load_tables(nodes=n, edgesets=e)
 
 print(ts)
 
