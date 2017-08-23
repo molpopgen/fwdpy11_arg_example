30ba2fed7a015cdb294c4998ea30c734e428885d
molpopgen <krthornt@uci.edu>
2017-08-23 13:41:51 -0700

    got children_length working here

--- practice/make_trees.py
+++ practice/make_trees.py
@@ -17,20 +17,33 @@ n.set_columns(flags=sv, population=pv, time=tv)
 
 print(n)
 
+e = msprime.EdgesetTable()
+print("Jerome's example:")
+left = [0, 1]
+right = [1, 2]
+parent = [10, 11]
+children_length = [2, 3]
+children = [1, 2, 3, 4, 5]
+e.set_columns(left=left, right=right, parent=parent, children=children_length, children_length=children_length)
+print(e)
+
+
 left = [0.2, 0.0, 0.2, 0.8, 0.8, 0.0]
 right = [0.8, 0.2, 0.8, 1.0, 1.0, 0.2]
 parent = [3, 4, 4, 4, 5, 6]
 # children = [(0,2),(1,2),(1,3),(0,4),(0,4)]
-children = [(0, 2), (1, 2), (1, 3), (1, 2), (0, 4), (0, 4)]
+children = [0, 2, 1, 2, 1, 3, 1, 2, 0, 4, 0, 4]
+#children = [(0, 2), (1, 2), (1, 3), (1, 2), (0, 4), (0, 4)]
 
-e = msprime.EdgesetTable()
-
-# e.set_columns(left=left,right=right,parent=parent,children=children,children_length=len(children))
-for l, r, p, c in zip(left, right, parent, children):
-    e.add_row(left=l, right=r, parent=p, children=c)
+print([2]*len(children))
+e.set_columns(left=left,right=right,parent=parent,children=children,children_length=[2]*len(parent))
+# for l, r, p, c in zip(left, right, parent, children):
+#     e.add_row(left=l, right=r, parent=p, children=c)
 
 
 print(e)
+
+
 
 msprime.sort_tables(nodes=n, edgesets=e)
 
