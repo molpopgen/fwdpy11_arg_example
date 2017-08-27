8357b100f5b585fbe69a2a8dcd57ecc0b633f253
molpopgen <krthornt@uci.edu>
2017-08-27 16:13:48 -0700

    add comment

--- practice/sorting.py
+++ practice/sorting.py
@@ -17,6 +17,7 @@ right = [0.8, 0.2, 0.8, 1.0, 1.0, 0.2]
 parent = [3, 4, 4, 4, 5, 6]
 children = [0, 2, 1, 2, 1, 3, 1, 2, 0, 4, 0, 4]
 
+#We swap some data around, but the tree is actuall the same.
 left2 = [0.2, 0.0, 0.8, 0.2, 0.8, 0.0]
 right2 = [0.8, 0.2, 1.0, 0.8, 1.0, 0.2]
 children2 = [0, 2,
