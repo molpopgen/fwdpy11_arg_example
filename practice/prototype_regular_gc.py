cb2f41578a7f362535d8e2186b34472a6259cf58
molpopgen <krthornt@uci.edu>
2017-09-01 11:40:54 -0700

    argparse and cleanup

--- practice/prototype_regular_gc.py
+++ practice/prototype_regular_gc.py
@@ -14,6 +14,7 @@
 import numpy as np
 import msprime
 import sys
+import argparse
 
 node_dt = np.dtype([('id', np.uint32),
                     ('generation', np.float),
@@ -73,7 +74,7 @@ class MockAncestryTracker(object):
     def convert_time(self):
         self.nodes['generation'] -= self.nodes['generation'].max()
         self.nodes['generation'] *= -1.0
-    
+
     def reset_data(self):
         """
         Call this after garbage collection.
@@ -82,7 +83,7 @@ class MockAncestryTracker(object):
         self.nodes = np.empty([0], dtype=node_dt)
         self.edges = np.empty([0], dtype=edge_dt)
 
-    def post_gc_cleanup(self,gc_rv):
+    def post_gc_cleanup(self, gc_rv):
         if gc_rv[0] is True:
             self.reset_data()
 
@@ -90,14 +91,14 @@ class MockAncestryTracker(object):
 class ARGsimplifier(object):
     """
     Mimicking the API if our Python
-    class to collect simulated 
+    class to collect simulated
     results and process them via msprime
     """
     __nodes = None
     __edges = None
     __gc_interval = None
 
-    def __init__(self,gc_interval=None):
+    def __init__(self, gc_interval=None):
         self.__nodes = msprime.NodeTable()
         self.__edges = msprime.EdgesetTable()
         self.gc_interval = gc_interval
@@ -121,37 +122,26 @@ class ARGsimplifier(object):
         flags.fill(1)
 
         tracker.convert_time()
-        tmin,tmax = None,None
-        if self.nodes.num_rows > 0:
-                tmin = self.nodes.time.min()
-                tmax = self.nodes.time.max()
-        # print(tmin,tmax,tracker.nodes['generation'].min(),tracker.nodes['generation'].max())
-        # print(self.nodes.num_rows,tracker.nodes['id'].min())
 
-        nl = self.nodes.num_rows
         self.nodes.append_columns(flags=flags,
                                   population=tracker.nodes['population'],
                                   time=tracker.nodes['generation'])
-        # if nl > 0:
-        #     print(self.nodes)
-        nl2 = self.nodes.num_rows
         self.edges.append_columns(left=tracker.edges['left'],
                                   right=tracker.edges['right'],
                                   parent=tracker.edges['parent'],
                                   children=tracker.edges['child'],
                                   children_length=[1] * len(tracker.edges))
 
-        msprime.sort_tables(nodes=self.nodes, edgesets = self.edges)
+        msprime.sort_tables(nodes=self.nodes, edgesets=self.edges)
         msprime.simplify_tables(samples=tracker.samples.tolist(),
                                 nodes=self.nodes, edgesets=self.edges)
-        # print(nl,nl2,self.nodes.num_rows)
         return self.nodes.num_rows
 
     def __call__(self, generation, tracker):
         if generation > 0 and generation % self.gc_interval == 0:
-            return (True,self.simplify(tracker))
+            return (True, self.simplify(tracker))
 
-        return (False,None)
+        return (False, None)
 
     @property
     def gc_interval(self):
@@ -256,11 +246,7 @@ def wf(N, simplifier, tracker, recrate, ngens):
     # so we pre-allocate the space. We only need
     # to do this once b/c N is constant.
     tracker.nodes = np.array([(i, 0, 0) for i in diploids], dtype=node_dt)
-    # tracker.nodes = np.empty([2 * N * (ngens + 1)], dtype=node_dt)
-    # tracker.nodes['id'][:len(diploids)] = diploids
-    # tracker.nodes['generation'][:len(diploids)] = 0.0
 
-    #node_id = int(len(diploids))
     next_id = len(diploids)  # This will be the next unique ID to use
     assert(max(diploids) < next_id)
     for gen in range(ngens):
@@ -268,7 +254,7 @@ def wf(N, simplifier, tracker, recrate, ngens):
         tracker.post_gc_cleanup(gc_rv)
         if gc_rv[0] is True:
             next_id = int(gc_rv[1])
-            diploids = np.arange(2*N, dtype = np.uint32)
+            diploids = np.arange(2 * N, dtype=np.uint32)
 
         # Empty offspring list.
         # We also use this to mark the "samples" for simplify
@@ -314,10 +300,6 @@ def wf(N, simplifier, tracker, recrate, ngens):
             # offspring dip, chrom 1:
             new_diploids[2 * dip] = next_id
 
-            # Add new node for offpsring dip, chrom 1
-            # nodes = np.insert(nodes, len(nodes), (next_id, gen + 1, 0))
-            #tracker.nodes[node_id] = (next_id, gen + 1, 0)
-
             # Repeat process for parent 2's contribution.
             # Stuff is now being inherited by node next_id + 1
             breakpoints = xover(recrate)
@@ -326,13 +308,10 @@ def wf(N, simplifier, tracker, recrate, ngens):
 
             new_diploids[2 * dip + 1] = next_id + 1
 
-            # nodes = np.insert(nodes, len(nodes), (next_id + 1, gen + 1, 0))
-
             # Update our dummy variables.
             next_id += 2
             dip += 1
 
-        # print(len(tracker.nodes),len(tracker.edges))
         assert(dip == N)
         assert(len(new_diploids) == 2 * N)
         diploids = new_diploids
@@ -342,136 +321,56 @@ def wf(N, simplifier, tracker, recrate, ngens):
     return (diploids)
 
 
-def expensive_check(popsize, edges, nodes):
-    """
-    A brute-force post-hoc check of the
-    nodes and edges that we generated
-    in the simulation
-    """
-    assert(len(edges) == SIMLEN * popsize * 4 * popsize)
+def parse_args():
+    dstring = "Prototype implementation of ARG tracking and regular garbage collection."
+    parser = argparse.ArgumentParser(description=dstring,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--popsize', '-N', type=int,
+                        default=500, help="Diploid population size")
+    parser.add_argument('--theta', '-T', type=float, default=10.0, help="4Nu")
+    parser.add_argument('--rho', '-R', type=float, default=10.0, help="4Nr")
+    parser.add_argument('--nsam', '-n', type=int, default=10,
+                        help="Sample size (in chromosomes).")
+    parser.add_argument('--seed', '-S', type=int, default=42, help="RNG seed")
+    parser.add_argument('--gc', '-G', type=int,
+                        default=100, help="GC interval")
 
-    # Check that all parent/child IDs are
-    # in expected range.
-    for gen in range(1, SIMLEN * popsize + 1):
-        min_parental_id = 2 * popsize * (gen - 1)
-        max_parental_id = 2 * popsize * (gen - 1) + 2 * popsize
-        min_child_id = max_parental_id
-        max_child_id = min_child_id + 2 * popsize
-
-        node_gen_m1 = nodes[np.argwhere(
-            nodes['generation'] == float(gen - 1)).flatten()]
-        assert(len(node_gen_m1) == 2 * popsize)
-        if any(i < min_parental_id or i >= max_parental_id for i in node_gen_m1['id']) is True:
-            raise RuntimeError("generation", gen - 1, "confused")
-
-        node_gen = nodes[np.argwhere(
-            nodes['generation'] == float(gen)).flatten()]
-        assert(len(node_gen) == 2 * popsize)
-        if any(i < min_child_id or i >= max_child_id for i in node_gen['id']) is True:
-            raise RuntimeError("generation", gen, "confused")
-
-        edges_gen = edges[(gen - 1) * 4 * popsize:(gen - 1)
-                          * 4 * popsize + 4 * popsize]
-
-        if any(i not in node_gen_m1['id'] for i in edges_gen['parent']) is True:
-            raise RuntimeError(
-                "parent not found in expected slice of node table")
-
-        if any(i not in node_gen['id'] for i in edges_gen['child']) is True:
-            raise RuntimeError(
-                "child not found in expected slice of node table")
-
-        if any(i < min_parental_id or i >= max_parental_id for i in edges_gen['parent']) is True:
-            raise RuntimeError("Bad parent")
-
-        if any(i < min_child_id or i >= max_child_id for i in edges_gen['child']) is True:
-            raise RuntimeError("Bad child")
+    return parser
 
 
 if __name__ == "__main__":
-    popsize = int(sys.argv[1])
-    theta = float(sys.argv[2])
-    rho = float(sys.argv[3])
-    nsam = int(sys.argv[4])  # sample size to take and add mutations to
-    seed = int(sys.argv[5])
+    parser = parse_args()
+    args = parser.parse_args(sys.argv[1:])
 
-    np.random.seed(seed)
+    np.random.seed(args.seed)
 
-    simplifier = ARGsimplifier(100)
+    simplifier = ARGsimplifier(args.gc)
     tracker = MockAncestryTracker()
-    recrate = rho / float(4 * popsize)
-    samples = wf(popsize, simplifier,tracker, recrate, SIMLEN * popsize)
+    recrate = args.rho / float(4 * args.popsize)
+    samples = wf(args.popsize, simplifier, tracker,
+                 recrate, SIMLEN * args.popsize)
 
-    # Check that our sample IDs are as expected:
-    # if __debug__:
-    #     min_sample = SIMLEN * popsize * 2 * popsize
-    #     max_sample = SIMLEN * popsize * 2 * popsize + 2 * popsize
-    #     if any(i < min_sample or i >= max_sample for i in samples) is True:
-    #         raise RuntimeError("Houston, we have a problem.")
-    # assert(np.array_equal(samples, np.arange(
-    #     min_sample, max_sample, dtype=samples.dtype)))
+    if len(tracker.nodes) > 0:  # Then there's stuff that didn't get GC'd
+        simplifier.simplify(tracker)
 
-    # Make local names for convenience
-    nodes = tracker.nodes
-    edges = tracker.edges
-    # print(len(edges))
-    # if __debug__:
-    #     expensive_check(popsize, edges, nodes)
+    # Local names for convenience.
+    # I copy the tables here, too,
+    # because I think that will be
+    # done in practice: you will
+    # often want to simplify and
+    # ARG down to a smaller sample
+    # but still have the complete
+    # history of the pop'n.
+    nodes = simplifier.nodes.copy()
+    edges = simplifier.edges.copy()
 
-    max_gen = nodes['generation'].max()
-    assert(int(max_gen) == SIMLEN * popsize)
-
-    # Convert node times from forwards to backwards
-    nodes['generation'] = nodes['generation'] - max_gen
-    nodes['generation'] = nodes['generation'] * -1.0
-
-    # Construct and populate msprime's tables
-    flags = np.empty([len(nodes)], dtype=np.uint32)
-    flags.fill(1)
-    nt = msprime.NodeTable()
-    nt.set_columns(flags=flags,
-                   population=nodes['population'],
-                   time=nodes['generation'])
-
-    es = msprime.EdgesetTable()
-    es.set_columns(left=edges['left'],
-                   right=edges['right'],
-                   parent=edges['parent'],
-                   children=edges['child'],
-                   children_length=[1] * len(edges))
-
-    # Sort
-    msprime.sort_tables(nodes=nt, edgesets=es)
-
-    # Simplify: this is where the magic happens
-    # PLR: since these tables aren't valid, you gotta use simplify_tables, not load them into a tree sequence
-    msprime.simplify_tables(samples=samples.tolist(), nodes=nt, edgesets=es)
-
-    # Create a tree sequence
-    x = msprime.load_tables(nodes=nt, edgesets=es)
-
-    # Lets look at the MRCAS.
-    # This is where things go badly:
-    MRCAS = [t.get_time(t.get_root()) for t in x.trees()]
-    # print(MRCAS)
-    # Throw down some mutations
-    # onto a sample of size nsam
-    # We'll copy tables here,
-    # just to see what happens.
-    # PLR: these .copy()s aren't doing anything: just overwritten before
-    nt_s = nt.copy()
-    es_s = es.copy()
-
-    nsam_samples = np.random.choice(2 * popsize, nsam, replace=False)
-    # PLR: TreeSequence.simplify() *returns* the modified tree sequence, leaving x unmodified
-    # you could alternatively do everything here with tables
-    xs = x.simplify(nsam_samples.tolist())
-    xs.dump_tables(nodes=nt_s, edgesets=es_s)
-    msp_rng = msprime.RandomGenerator(seed)
+    nsam_samples = np.random.choice(2 * args.popsize, args.nsam, replace=False)
+    msprime.simplify_tables(samples=nsam_samples.tolist(),
+                            nodes=nodes, edgesets=edges)
+    msp_rng = msprime.RandomGenerator(args.seed)
     mutations = msprime.MutationTable()
     sites = msprime.SiteTable()
-    mutgen = msprime.MutationGenerator(msp_rng, theta / float(4 * popsize))
-    mutgen.generate(nt_s, es_s, sites, mutations)
-    x = msprime.load_tables(nodes=nt_s, edgesets=es_s,
-                            sites=sites, mutations=mutations)
+    mutgen = msprime.MutationGenerator(
+        msp_rng, args.theta / float(4 * args.popsize))
+    mutgen.generate(nodes, edges, sites, mutations)
     print(sites.num_rows)
