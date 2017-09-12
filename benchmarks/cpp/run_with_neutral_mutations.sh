572aa8aa6700656c06ebeae3d688217917d7fb62
molpopgen <krthornt@uci.edu>
2017-09-12 15:54:19 -0700

    fix script to simulate neutral mutations

--- benchmarks/cpp/run_with_neutral_mutations.sh
+++ benchmarks/cpp/run_with_neutral_mutations.sh
@@ -11,6 +11,6 @@ do
         GC=`echo "0.1*$N"|bc -l| sed 's/\.0//'`
         TIME_MEM_FILE=time_with_neutral.N$N"."size$size".out"
         DETAILED_TIME_FILE=detailed_time_with_neutral.N$N"."size$size".out"
-        /usr/bin/time -f "%e %M" -o $TIME_MEM_FILE parallel --timeout $SECONDS PYTHONPATH=../.. python ../../benchmarking.py --neutral --popsize $N --theta $size --rho $size --nsam 100 --pdel 0.1 --gc $GC --seed ::: 42 > $DETAILED_TIME_FILE
+        /usr/bin/time -f "%e %M" -o $TIME_MEM_FILE parallel --timeout $SECONDS PYTHONPATH=../.. python ../../benchmarking.py --neutral_mutations --popsize $N --theta $size --rho $size --nsam 100 --pdel 0.1 --gc $GC --seed ::: 42 > $DETAILED_TIME_FILE
     done
 done
