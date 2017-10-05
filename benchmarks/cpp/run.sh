8e7b4a0f7bb6887a48b6c906622f42a11b00a54c
molpopgen <krthornt@uci.edu>
2017-10-05 10:23:18 -0700

    oops, SECONDS is a bash keyword

--- benchmarks/cpp/run.sh
+++ benchmarks/cpp/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 72 hour run limit
-SECONDS=`echo "72*60*60"|bc -l`
+SECONDS_TO_KILL=`echo "72*60*60"|bc -l`
 
 for N in 1000 10000 100000
 do
@@ -12,6 +12,6 @@ do
         GC=1000
         TIME_MEM_FILE=time_arg.N$N"."size$size".out"
         DETAILED_TIME_FILE=detailed_time_arg.N$N"."size$size".out"
-        /usr/bin/time -f "%e %M" -o $TIME_MEM_FILE parallel --timeout $SECONDS PYTHONPATH=../.. python ../../benchmarking.py --popsize $N --theta $size --rho $size --nsam 100 --pdel 0.01 --gc $GC --seed ::: 42 > $DETAILED_TIME_FILE
+        /usr/bin/time -f "%e %M" -o $TIME_MEM_FILE parallel --timeout $SECONDS_TO_KILL PYTHONPATH=../.. python ../../benchmarking.py --popsize $N --theta $size --rho $size --nsam 100 --pdel 0.01 --gc $GC --seed ::: 42 > $DETAILED_TIME_FILE &
     done
 done
--- benchmarks/cpp/run_with_neutral_mutations.sh
+++ benchmarks/cpp/run_with_neutral_mutations.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # 72 hour run limit
-SECONDS=`echo "72*60*60"|bc -l`
+SECONDS_TO_KILL=259200
 
+export PYTHONPATH=../.. 
 for N in 1000 10000 100000
 do
     for size in 1000 10000 100000
     do
         #We will arbitrarily GC every 0.1N generations
-        GC=`echo "0.1*$N"|bc -l| sed 's/\.0//'`
+        GC=1000
         TIME_MEM_FILE=time_with_neutral.N$N"."size$size".out"
-        DETAILED_TIME_FILE=detailed_time_with_neutral.N$N"."size$size".out"
-        /usr/bin/time -f "%e %M" -o $TIME_MEM_FILE parallel --timeout $SECONDS PYTHONPATH=../.. python ../../benchmarking.py --neutral_mutations --popsize $N --theta $size --rho $size --nsam 100 --pdel 0.01 --gc $GC --seed ::: 42 > $DETAILED_TIME_FILE
+        if [ ! -e $TIME_MEM_FILE ]
+        then
+            echo "SECONDS_TO_KILL = $SECONDS_TO_KILL"
+            /usr/bin/time -f "%e %M" -o $TIME_MEM_FILE timeout $SECONDS_TO_KILL python ../../benchmarking.py --neutral_mutations --popsize $N --theta $size --rho $size --nsam 100 --pdel 0.01 --gc $GC --seed 42 &
+        fi
     done
 done
