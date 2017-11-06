1ae7f13310c8e36d82ff9f8ef1852d55499e985c
molpopgen <krthornt@uci.edu>
2017-11-06 11:54:36 -0800

    updates to benchmark scripts

--- benchmarks/cpp/run.sh
+++ benchmarks/cpp/run.sh
@@ -3,7 +3,7 @@
 # 72 hour run limit
 SECONDS_TO_KILL=`echo "72*60*60"|bc -l`
 
-for N in 1000 10000 100000
+for N in 1000 10000 50000
 do
     for size in 1000 10000 100000
     do
@@ -11,7 +11,7 @@ do
         GC=`echo "0.1*$N"|bc -l| sed 's/\.0//'`
         GC=1000
         TIME_MEM_FILE=time_arg.N$N"."size$size".out"
-        DETAILED_TIME_FILE=detailed_time_arg.N$N"."size$size".out"
-        /usr/bin/time -f "%e %M" -o $TIME_MEM_FILE parallel --timeout $SECONDS_TO_KILL PYTHONPATH=../.. python ../../benchmarking.py --popsize $N --theta $size --rho $size --nsam 100 --pdel 0.01 --gc $GC --seed ::: 42 > $DETAILED_TIME_FILE &
+        DETAILED_TIME_FILE=detailed_time_arg.N$N"."size$size".out.gz"
+        /usr/bin/time -f "%e %M" -o $TIME_MEM_FILE parallel --timeout $SECONDS_TO_KILL PYTHONPATH=../.. python ../../benchmarking.py --popsize $N --theta $size --rho $size --nsam 100 --pdel 0.01 --gc $GC --outfile1 $DETAILED_TIME_FILE --seed ::: 42 
     done
 done
--- benchmarks/cpp/run_with_neutral_mutations.sh
+++ benchmarks/cpp/run_with_neutral_mutations.sh
@@ -4,17 +4,31 @@
 SECONDS_TO_KILL=259200
 
 export PYTHONPATH=../.. 
-for N in 1000 10000 100000
+
+for N in 1000 10000 50000
 do
+    KILL=0
     for size in 1000 10000 100000
     do
         #We will arbitrarily GC every 0.1N generations
+        echo "PARAMS = " $N $size
         GC=1000
         TIME_MEM_FILE=time_with_neutral.N$N"."size$size".out"
-        if [ ! -e $TIME_MEM_FILE ]
+        if [ $KILL == 0 ]
         then
-            echo "SECONDS_TO_KILL = $SECONDS_TO_KILL"
-            /usr/bin/time -f "%e %M" -o $TIME_MEM_FILE timeout $SECONDS_TO_KILL python ../../benchmarking.py --neutral_mutations --popsize $N --theta $size --rho $size --nsam 100 --pdel 0.01 --gc $GC --seed 42 &
+            DETAILED_TIME_FILE=detailed_time_argi_with_neutral.N$N"."size$size".out.gz"
+            /usr/bin/time -f "%e %M" -o $TIME_MEM_FILE parallel --timeout $SECONDS_TO_KILL PYTHONPATH=../.. python ../../benchmarking.py --popsize $N --neutral_mutations --theta $size --rho $size --nsam 100 --pdel 0.01 --gc $GC --outfile1 $DETAILED_TIME_FILE --seed ::: 42 
+            # If we terminated the job,
+            # then the next params will
+            # also take too long, and 
+            # so we can skip running them
+            STATUS=$?
+            if [ $STATUS -gt 0 ]
+            then
+                echo "STATUS = " $STATUS
+                KILL=1
+            fi
         fi
+            #/usr/bin/time -f "%e %M" -o $TIME_MEM_FILE timeout $SECONDS_TO_KILL python ../../benchmarking.py --neutral_mutations --popsize $N --theta $size --rho $size --nsam 100 --pdel 0.01 --gc $GC --seed 42
     done
 done
