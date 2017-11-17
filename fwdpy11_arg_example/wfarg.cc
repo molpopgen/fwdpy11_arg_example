d17276cd3b42c6a59d350d7e0bf5747ca717f766
molpopgen <krthornt@uci.edu>
2017-11-17 13:35:29 -0800

    new rules object in place where we attempt to parallelize fitness calculations

--- fwdpy11_arg_example/wfarg.cc
+++ fwdpy11_arg_example/wfarg.cc
@@ -116,6 +116,90 @@ evolve_singlepop_regions_track_ancestry(
     return time_simulating;
 }
 
+struct wf_rules_async_fitness : public fwdpy11::wf_rules
+{
+    int nthreads;
+    std::vector<std::future<double>> fitness_futures;
+    struct accumulate_fitnesses
+    // This version directly fills in the fitness vector.
+    // The potential issue with this approach is contention
+    // due to false sharing at the boundaries of ranges
+    // updated by different threads.
+    {
+        template <typename itr, typename gcont, typename mcont, typename wfunc>
+        inline double
+        operator()(const gcont& gametes, const mcont& mutations,
+                   std::vector<double>& w, std::size_t i, itr beg, itr end,
+                   const wfunc& ff)
+        {
+            double sum = 0.0;
+            for (; beg < end; ++beg, ++i)
+                {
+                    double result = ff(*beg, gametes, mutations);
+                    beg->w = result;
+                    beg->g = result;
+                    w[i] = result;
+                    sum += result;
+                }
+            return sum;
+        }
+    };
+
+    wf_rules_async_fitness(int nthreads_ = 1)
+        : wf_rules(), nthreads(nthreads_), fitness_futures{}
+    {
+        if (nthreads > 1)
+            {
+                fitness_futures.resize(nthreads - 1);
+            }
+    }
+
+    double
+    w(fwdpy11::singlepop_t& pop, const fwdpy11::single_locus_fitness_fxn& ff)
+    {
+        double wbar = 0.0;
+        if (nthreads > 1)
+            {
+                auto N_curr = pop.diploids.size();
+                std::size_t increment = N_curr / nthreads + 1;
+                std::size_t offset = increment;
+                auto first_diploid = pop.diploids.begin() + offset;
+                for (std::size_t i = 0;
+                     i < static_cast<std::size_t>(nthreads) - 1; ++i)
+                    {
+                        auto last_diploid_in_range = std::min(
+                            pop.diploids.end(), first_diploid + increment);
+                        // operator()(const gcont& gametes, const mcont& mutations,
+                        //            std::vector<double>& w, std::size_t i, itr beg, itr end,
+                        //            const wfunc& ff)
+                        fitness_futures[i] = std::async(
+                            std::launch::async,
+                            std::bind(
+                                accumulate_fitnesses(), std::cref(pop.gametes),
+                                std::cref(pop.mutations), std::ref(fitnesses),
+                                static_cast<std::size_t>(std::distance(
+                                    pop.diploids.begin(), first_diploid)),
+                                first_diploid, last_diploid_in_range, ff));
+                    }
+                wbar = accumulate_fitnesses()(
+                    pop.gametes, pop.mutations, fitnesses, 0,
+                    pop.diploids.begin(), pop.diploids.begin() + offset, ff);
+                for (auto& f : fitness_futures)
+                    {
+                        wbar += f.get();
+                    }
+                wbar /= static_cast<double>(N_curr);
+                lookup = KTfwd::fwdpp_internal::gsl_ran_discrete_t_ptr(
+                    gsl_ran_discrete_preproc(N_curr, &fitnesses[0]));
+            }
+        else
+            {
+                fwdpy11::wf_rules::w(pop, ff);
+            }
+        return wbar;
+    }
+};
+
 double
 evolve_singlepop_regions_track_ancestry_async(
     const fwdpy11::GSLrng_t& rng, fwdpy11::singlepop_t& pop,
@@ -154,7 +238,7 @@ evolve_singlepop_regions_track_ancestry_async(
         mmodel, pop.mutations, pop.mut_lookup, rng.get(), 0.0, mu_selected,
         &pop.generation);
     ++pop.generation;
-    auto rules = fwdpy11::wf_rules();
+    auto rules = wf_rules_async_fitness();
 
     auto fitness_callback = fitness.callback();
     fitness.update(pop);
@@ -210,12 +294,12 @@ evolve_singlepop_regions_track_ancestry_async(
             auto start = std::clock();
             evolve_generation(
                 rng, pop, N_next, mu_selected, mmodels, recmap,
-                std::bind(&fwdpy11::wf_rules::pick1, &rules,
+                std::bind(&decltype(rules)::pick1, &rules,
                           std::placeholders::_1, std::placeholders::_2),
-                std::bind(&fwdpy11::wf_rules::pick2, &rules,
+                std::bind(&decltype(rules)::wf_rules::pick2, &rules,
                           std::placeholders::_1, std::placeholders::_2,
                           std::placeholders::_3, selfing_rate),
-                std::bind(&fwdpy11::wf_rules::update, &rules,
+                std::bind(&decltype(rules)::update, &rules,
                           std::placeholders::_1, std::placeholders::_2,
                           std::placeholders::_3, std::placeholders::_4,
                           std::placeholders::_5),
