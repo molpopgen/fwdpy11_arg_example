ae251b1e66c7dc90b6a62b19d82b8e2d0ccdebad
molpopgen <krthornt@uci.edu>
2017-11-16 16:27:11 -0800

    remove checking code

--- fwdpy11_arg_example/argsimplifier.py
+++ fwdpy11_arg_example/argsimplifier.py
@@ -56,22 +56,23 @@ class ArgSimplifier(object):
             # This next step does not have to be done.
             # It is here for checking now.  Can delete
             # later.
-            na['id'] -= delta
+            # na['id'] -= delta
             # print("updating IDs:",new_min_id,na['id'][0],delta)
             # print("max IDs are: ",na['id'].min(),na['id'].max())
             # print("new edges",ea)
-            for field in ['parent','child']:
-                eids = np.where((ea[field]>=new_min_id)&(ea[field]<=new_max_id))[0]
+            # for field in ['parent','child']:
+            eids = np.where((ea['parent']>=new_min_id)&(ea['parent']<=new_max_id))[0]
                 # print("processing ",field,len(eids),len(self.__nodes),len(na),new_min_id,new_max_id)
-                ea[field][eids] -= delta
-                sd = np.setdiff1d(ea[field],na['id'])
+            ea['parent'][eids] -= delta
+            ea['child'] -= delta
+                # sd = np.setdiff1d(ea[field],na['id'])
                 # print("checking",field)
                 # for x in sd:
                 #     assert(x < len(self.__nodes)), "Value out of bounds {}".format(x)
             samples -= delta
             # print(samples)
-            sdiff = np.setdiff1d(samples,na['id'])
-            assert(len(sdiff) == 0)
+            # sdiff = np.setdiff1d(samples,na['id'])
+            # assert(len(sdiff) == 0)
             # print("new edges 2",ea)
         # print(samples)
         flags = np.ones(len(na), dtype=np.uint32)
