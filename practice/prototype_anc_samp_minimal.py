96ed7c72f8b0db810d8bc7605c680b56d9db3536
David Lawrie <dlawrie@imac.attlocal.net>
2018-02-01 19:27:21 -0800

    removed SIMLEN, replaced with just ngens

--- practice/prototype_anc_samp_minimal.py
+++ practice/prototype_anc_samp_minimal.py
@@ -6,7 +6,7 @@ import sys
 import argparse
 
 # Simulation constants
-popsize, SIMLEN, nsam, seed, gc_interval = 500, 20, 5, 42, 100
+popsize, ngens, nsam, seed, gc_interval = 500, 20*500, 5, 42, 100
 
 class ARGsimplifier(object):
     """
@@ -14,13 +14,13 @@ class ARGsimplifier(object):
     class to collect simulated
     results and process them via msprime
     """
-    def __init__(self, ngens):
+    def __init__(self):
         self.nodes, self.edges = msprime.NodeTable(), msprime.EdgeTable()
         prior_ts = msprime.simulate(sample_size=2*popsize, Ne=2*popsize, random_seed=seed)
         prior_ts.dump_tables(nodes=self.nodes, edges=self.edges)
         self.nodes.set_columns(flags=self.nodes.flags, population=self.nodes.population, time=self.nodes.time + ngens)
 
-    def simplify(self, generation, ngens, samples, anc_samples):
+    def simplify(self, generation, samples, anc_samples):
         """
         Details of simplifying.
 
@@ -39,7 +39,7 @@ class ARGsimplifier(object):
             # Return length of NodeTable, which can be used as next offspring ID and reset diploids (parent gen to 0, 2N)
         return self.nodes.num_rows, diploids, diploids + self.nodes.num_rows, anc_samples
 
-def wf(N, simplifier, anc_sample_gen, ngens):
+def wf(N, simplifier, anc_sample_gen):
     """
     For N diploids, the diploids list contains 2N values.
     For the i-th diploids, diploids[2*i] and diploids[2*i+1]
@@ -54,7 +54,7 @@ def wf(N, simplifier, anc_sample_gen, ngens):
     3. We pass one parental index on to each offspring, according to
        Mendel, which means we swap parental chromosome ids 50% of the time.
     """
-    next_id, diploids, new_diploids, ancestral_samples = simplifier.simplify(0, ngens, np.empty([0], dtype=np.uint32), np.empty([0], dtype=np.uint32))  #  Based on prior history, this will be the next unique ID to use
+    next_id, diploids, new_diploids, ancestral_samples = simplifier.simplify(0, np.empty([0], dtype=np.uint32), np.empty([0], dtype=np.uint32))  #  Based on prior history, this will be the next unique ID to use
     ancestral_gen_counter = 0
     for gen in range(ngens):
         if(ancestral_gen_counter < len(anc_sample_gen) and gen + 1 == anc_sample_gen[ancestral_gen_counter][0]):
@@ -90,14 +90,13 @@ def wf(N, simplifier, anc_sample_gen, ngens):
             # length of the current NodeTable,
             # keeping risk of integer overflow
             # to a minimum.
-            next_id, diploids, new_diploids, ancestral_samples = simplifier.simplify(gen, ngens, diploids, ancestral_samples)
+            next_id, diploids, new_diploids, ancestral_samples = simplifier.simplify(gen, diploids, ancestral_samples)
     return diploids, ancestral_samples
     
 if __name__ == "__main__":
-    ngens = SIMLEN * popsize
-    simplifier = ARGsimplifier(ngens)
-    anc_sample_gen = [(ngens * (i + 1) / SIMLEN, max(round(popsize / 200), 1)) for i in range(SIMLEN - 2)]
-    samples, anc_samples = wf(popsize, simplifier, anc_sample_gen, ngens)
+    simplifier = ARGsimplifier()
+    anc_sample_gen = [(ngens * (i + 1) / 20, max(round(popsize / 200), 1)) for i in range(20 - 2)]
+    samples, anc_samples = wf(popsize, simplifier, anc_sample_gen)
     ran_samples = np.random.choice(popsize, nsam, replace=False)
     nsam_samples = sorted(np.concatenate((2*ran_samples, 2*ran_samples + 1)))
     all_samples = nsam_samples + anc_samples.tolist()
