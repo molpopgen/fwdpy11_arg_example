d89acc454d851d268cb631b6193f215266967233
David Lawrie <dlawrie@imac.attlocal.net>
2018-02-01 19:18:07 -0800

    removed parse_args() and parser, made them simulation constants

--- practice/prototype_anc_samp_minimal.py
+++ practice/prototype_anc_samp_minimal.py
@@ -6,8 +6,8 @@ import msprime
 import sys
 import argparse
 
-# Simulation with be popsize*SIMLEN generations
-SIMLEN = 20
+# Simulation constants
+popsize, SIMLEN, nsam, seed, gc = 500, 20, 5, 42, 100
 
 class ARGsimplifier(object):
     """
@@ -15,12 +15,12 @@ class ARGsimplifier(object):
     class to collect simulated
     results and process them via msprime
     """
-    def __init__(self, ngens, gc_interval=None):
+    def __init__(self, ngens):
         self.nodes, self.edges = msprime.NodeTable(), msprime.EdgeTable()
-        prior_ts = msprime.simulate(sample_size=2 * args.popsize, Ne=2 * args.popsize, random_seed=args.seed)
+        prior_ts = msprime.simulate(sample_size=2*popsize, Ne=2*popsize, random_seed=seed)
         prior_ts.dump_tables(nodes=self.nodes, edges=self.edges)
         self.nodes.set_columns(flags=self.nodes.flags, population=self.nodes.population, time=self.nodes.time + ngens)
-        self.gc_interval = gc_interval
+        self.gc_interval = gc
 
     def simplify(self, generation, ngens, samples, anc_samples):
         """
@@ -28,7 +28,7 @@ class ARGsimplifier(object):
 
         :return: length of simplifed node table, which is next_id to use
         """
-        diploids = np.arange(2 * args.popsize, dtype=np.uint32)
+        diploids = np.arange(2*popsize, dtype=np.uint32)
         if(generation > 0): 
             # Sort and simplify
             msprime.sort_tables(nodes=self.nodes, edges=self.edges)
@@ -95,24 +95,12 @@ def wf(N, simplifier, anc_sample_gen, ngens):
             next_id, diploids, new_diploids, ancestral_samples = simplifier.simplify(gen, ngens, diploids, ancestral_samples)
     return diploids, ancestral_samples
     
-def parse_args():
-    dstring = "Prototype implementation of ARG tracking and regular garbage collection."
-    parser = argparse.ArgumentParser(description=dstring, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--popsize', '-N', type=int, default=500, help="Diploid population size")
-    parser.add_argument('--nsam', '-n', type=int, default=5, help="Sample size (in diploids).")
-    parser.add_argument('--seed', '-S', type=int, default=42, help="RNG seed")
-    parser.add_argument('--gc', '-G', type=int, default=100, help="GC interval")  
-    return parser
-
 if __name__ == "__main__":
-    parser = parse_args()
-    args = parser.parse_args(sys.argv[1:])
-    np.random.seed(args.seed)
-    ngens = SIMLEN * args.popsize
-    simplifier = ARGsimplifier(ngens,args.gc)
-    anc_sample_gen = [(ngens * (i + 1) / SIMLEN, max(round(args.popsize / 200), 1)) for i in range(SIMLEN - 2)]
-    samples, anc_samples = wf(args.popsize, simplifier, anc_sample_gen, ngens)
-    ran_samples = np.random.choice(args.popsize, args.nsam, replace=False)
+    ngens = SIMLEN * popsize
+    simplifier = ARGsimplifier(ngens)
+    anc_sample_gen = [(ngens * (i + 1) / SIMLEN, max(round(popsize / 200), 1)) for i in range(SIMLEN - 2)]
+    samples, anc_samples = wf(popsize, simplifier, anc_sample_gen, ngens)
+    ran_samples = np.random.choice(popsize, nsam, replace=False)
     nsam_samples = sorted(np.concatenate((2*ran_samples, 2*ran_samples + 1)))
     all_samples = nsam_samples + anc_samples.tolist()
     node_map = msprime.simplify_tables(samples=all_samples, nodes=simplifier.nodes, edges=simplifier.edges)
