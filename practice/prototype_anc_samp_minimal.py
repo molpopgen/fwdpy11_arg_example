76743a736aa18731d837a34addf95cfedd5a42a5
David Lawrie <dlawrie@imac.attlocal.net>
2018-02-01 20:08:22 -0800

    replaced N with popsize in wf and re-added setting np.random.seed to seed

--- practice/prototype_anc_samp_minimal.py
+++ practice/prototype_anc_samp_minimal.py
@@ -6,7 +6,7 @@ import sys
 import argparse
 
 # Simulation constants
-popsize, ngens, nsam, seed, anc_sample_interval, gc_interval = 500, 20*500, 5, 42, 500, 100
+popsize, ngens, nsam, seed, anc_sample_interval, gc_interval = int(500), int(20*500), int(5), int(42), int(500), int(100)
 
 class ARGsimplifier(object):
     """
@@ -39,16 +39,16 @@ class ARGsimplifier(object):
             # Return length of NodeTable, which can be used as next offspring ID and reset diploids (parent gen to 0, 2N)
         return self.nodes.num_rows, diploids, diploids + self.nodes.num_rows, anc_samples
 
-def wf(N, simplifier):
+def wf(simplifier):
     """
-    For N diploids, the diploids list contains 2N values.
+    For #popsize diploids, the diploids list contains 2*popsize values.
     For the i-th diploids, diploids[2*i] and diploids[2*i+1]
     represent the two chromosomes.
 
-    We simulate constant N here, according to a Wright-Fisher
+    We simulate constant popsize here, according to a Wright-Fisher
     scheme:
 
-    1. Pick two parental indexes, each [0,N-1]
+    1. Pick two parental indexes, each [0,popsize-1]
     2. The two chromosome ids are diploids[2*p] and diploids[2*p+1]
        for each parental index 'p'.
     3. We pass one parental index on to each offspring, according to
@@ -57,14 +57,14 @@ def wf(N, simplifier):
     next_id, diploids, new_diploids, ancestral_samples = simplifier.simplify(0, np.empty([0], dtype=np.uint32), np.empty([0], dtype=np.uint32))  #  Based on prior history, this will be the next unique ID to use
     for gen in range(ngens):
         if (((gen + 1) % anc_sample_interval) == 0) and ((gen+1) != ngens): #take 1 ancestral sample every anc_sample_interval generations
-            ran_samples = np.random.choice(int(N), int(1), replace=False)[0] 
+            ran_samples = np.random.choice(popsize, 1, replace=False)[0] 
             # while sorting to get diploid chromosomes next to each other isn't strictly necessary,
             # they will be sorted (in reverse order) before simplication anyway, no need to do it here
             ancestral_samples = np.insert(ancestral_samples, len(ancestral_samples), np.array([2*ran_samples + next_id, 2*ran_samples + next_id + 1],dtype=np.uint32))
         # Store nodes for this generation.
-        for i in range(2*N): simplifier.nodes.add_row(1,(ngens - (gen+1)),0)
-        # Pick 2N parents:
-        parents = np.random.randint(0, N, 2 * N)
+        for i in range(2*popsize): simplifier.nodes.add_row(1,(ngens - (gen+1)),0)
+        # Pick 2*popsize parents:
+        parents = np.random.randint(0, popsize, 2*popsize)
         # Iterate over our chosen parents via fancy indexing.
         for parent1, parent2 in zip(parents[::2], parents[1::2]):
             mendel = np.random.random_sample(2)
@@ -75,13 +75,13 @@ def wf(N, simplifier):
             # Update our dummy variable.
             next_id += 2
         diploids = new_diploids
-        new_diploids = diploids + 2*N
+        new_diploids = diploids + 2*popsize
         # Let's see if we will do some GC:
         if ((gen+1) % gc_interval == 0) or ((gen+1) == ngens):
             # If we do GC, we need to reset
             # some variables.  Internally,
             # when msprime simplifies tables,
-            # the 2N tips are entries 0 to 2*N-1
+            # the 2*popsize tips are entries 0 to 2*popsize-1
             # in the NodeTable, hence the
             # re-assignment of diploids.
             # We can also reset next_id to the
@@ -92,8 +92,9 @@ def wf(N, simplifier):
     return diploids, ancestral_samples
     
 if __name__ == "__main__":
+    np.random.seed(seed)
     simplifier = ARGsimplifier()
-    samples, anc_samples = wf(popsize, simplifier)
+    samples, anc_samples = wf(simplifier)
     ran_samples = np.random.choice(popsize, nsam, replace=False)
     nsam_samples = sorted(np.concatenate((2*ran_samples, 2*ran_samples + 1)))
     all_samples = nsam_samples + anc_samples.tolist()
