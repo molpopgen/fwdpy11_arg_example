molpopgen/fwdpy11_arg_example

commit 309f1096c7aa27677483142687ed402f7b1b17a4

subsume ancestry_tracker inside wright-fisher use ancestry_processor's num_nodes to set next_index (if next_index == 0, initialize a tracker with newly created "parent" nodes)

fwdpy11_arg_example/ancestry_tracker.hpp
@@ -79,8 +79,7 @@ struct ancestry_tracker
     integer_type generation, next_index, first_parental_index;
     std::uint32_t lastN;
     decltype(node::generation) last_gc_time;
-    ancestry_tracker(const integer_type N, const bool init_with_TreeSequence,
-                     const integer_type next_index_)
+    ancestry_tracker(const integer_type N, const integer_type next_index_)
         : nodes{ std::vector<node>() }, edges{ std::vector<edge>() },
           temp{ std::vector<edge>() }, mutations{ std::vector<mutation>() }, 
           offspring_indexes{ std::vector<integer_type>() }, generation{ 1 },
@@ -93,7 +92,8 @@ struct ancestry_tracker
         //no need to reserve mutation space
 
         //Initialize 2N nodes for the generation 0
-        if (init_with_TreeSequence == false)
+        //if next_index == 0, then did not initialize with tree sequence
+        if (next_index_ == 0)
             {
                 for (integer_type i = 0; i < 2 * N; ++i)
                     {

fwdpy11_arg_example/argsimplifier.py
@@ -106,7 +106,7 @@ class ArgSimplifier(object):
         self.__time_simplifying += time.process_time() - before
         self.__process = True
         return (True, self.__nodes.num_rows)
-
+        
     def __call__(self, pop, ancestry, override):
         """
         This is called from C++ during a simulation.
@@ -119,12 +119,10 @@ class ArgSimplifier(object):
 
         :returns: A bool and an int
         """
-        if len(ancestry.nodes) > 0 and len(ancestry.edges) > 0:
+        if ancestry is not None and len(ancestry.nodes) > 0 and len(ancestry.edges) > 0:
             if pop.generation > 0 and (pop.generation % self.gc_interval == 0.0 or override):
                 return self.simplify(pop, ancestry)
-        # Keep tuple size constant,
-        # for sake of sanity.
-        return (False, None)
+        return (False, self.__nodes.num_rows)
 
     @property
     def nodes(self):

fwdpy11_arg_example/evolve_arg.py
@@ -40,7 +40,7 @@ def evolve_track(rng, pop, params, gc_interval, init_with_TreeSequence=False, ms
     mm = makeMutationRegions(params.nregions, params.sregions)
     rm = makeRecombinationRegions(params.recregions)
 
-    from .wfarg import evolve_singlepop_regions_track_ancestry, AncestryTracker
+    from .wfarg import evolve_singlepop_regions_track_ancestry
     from .argsimplifier import ArgSimplifier
     initial_TreeSequence = None
     next_index = 2 * pop.N
@@ -51,15 +51,13 @@ def evolve_track(rng, pop, params, gc_interval, init_with_TreeSequence=False, ms
                 "msprime_seed is None. Results will not be reprodicible.")
         initial_TreeSequence = msprime.simulate(
             2 * pop.N, recombination_rate=params.recrate / 2.0, Ne=pop.N, random_seed=msprime_seed)
-        next_index = initial_TreeSequence.num_nodes
     simplifier = ArgSimplifier(gc_interval, initial_TreeSequence)
-    atracker = AncestryTracker(pop.N, init_with_TreeSequence, next_index)
-    tsim = evolve_singlepop_regions_track_ancestry(rng, pop, atracker, simplifier,
+    tsim = evolve_singlepop_regions_track_ancestry(rng, pop, simplifier,
                                                        params.demography,
                                                        params.mutrate_s,
                                                        params.recrate, mm, rm,
                                                        params.gvalue, params.pself)
-    return (simplifier, atracker, tsim)
+    return (simplifier, tsim)
 
 
 def evolve_track_wrapper(popsize=1000, rho=10000.0, mu=1e-2, seed=42,

fwdpy11_arg_example/evolve_functions.hpp
@@ -9,7 +9,7 @@
 double
 evolve_singlepop_regions_track_ancestry(
     const fwdpy11::GSLrng_t& rng, fwdpy11::singlepop_t& pop,
-    ancestry_tracker& ancestry, pybind11::function ancestry_processor,
+    pybind11::function ancestry_processor,
     pybind11::array_t<std::uint32_t> popsizes, const double mu_selected,
     const double recrate, const KTfwd::extensions::discrete_mut_model& mmodel,
     const KTfwd::extensions::discrete_rec_model& rmodel,

fwdpy11_arg_example/wfarg.cc
@@ -52,8 +52,8 @@ PYBIND11_MODULE(wfarg, m)
     //We only expose the stuff that a user really needs
     //to see.
     py::class_<ancestry_tracker>(m, "AncestryTracker")
-        .def(py::init<decltype(edge::parent), bool, decltype(edge::parent)>(),
-             py::arg("N"), py::arg("init_with_TreeSequence"),
+        .def(py::init<decltype(edge::parent), decltype(edge::parent)>(),
+             py::arg("N"), 
              py::arg("next_index"))
         .def(py::init<ancestry_tracker&>())
         .def_readwrite("nodes", &ancestry_tracker::nodes,

fwdpy11_arg_example/wright_fisher_single_thread.cc
@@ -25,9 +25,9 @@ namespace py = pybind11;
 double
 evolve_singlepop_regions_track_ancestry(
     const fwdpy11::GSLrng_t& rng, fwdpy11::singlepop_t& pop,
-    ancestry_tracker& ancestry, py::function ancestry_processor,
-    py::array_t<std::uint32_t> popsizes, const double mu_selected,
-    const double recrate, const KTfwd::extensions::discrete_mut_model& mmodel,
+    py::function ancestry_processor, py::array_t<std::uint32_t> popsizes, 
+    const double mu_selected, const double recrate, 
+    const KTfwd::extensions::discrete_mut_model& mmodel,
     const KTfwd::extensions::discrete_rec_model& rmodel,
     fwdpy11::single_locus_fitness& fitness, const double selfing_rate)
 {
@@ -37,6 +37,10 @@ evolve_singlepop_regions_track_ancestry(
                 "this population has already been evolved.");
         }
     const auto generations = popsizes.size();
+    py::tuple processor_rv = ancestry_processor(pop, nullptr, false);
+	int next_index = processor_rv[1].cast<int>();
+    ancestry_tracker ancestry(pop.N, next_index);
+    
     if (!generations)
         throw std::runtime_error("empty list of population sizes");
     if (mu_selected < 0.)
@@ -70,7 +74,7 @@ evolve_singlepop_regions_track_ancestry(
          ++generation, ++pop.generation)
         {
             //Ask if we need to garbage collect:
-            py::tuple processor_rv = ancestry_processor(pop, ancestry, false);
+            processor_rv = ancestry_processor(pop, ancestry, false);
             //If we did GC, then the ancestry_tracker has
             //some cleaning up to do:
             ancestry.post_process_gc(processor_rv);
@@ -102,7 +106,7 @@ evolve_singlepop_regions_track_ancestry(
             auto dur = (stop - start) / static_cast<double>(CLOCKS_PER_SEC);
             time_simulating += dur;
         }
-    py::tuple processor_rv = ancestry_processor(pop, ancestry, true);
+    processor_rv = ancestry_processor(pop, ancestry, true);
     --pop.generation;
     return time_simulating;
 }

test_evolve.py
@@ -8,7 +8,7 @@ rho = float(sys.argv[2])
 theta = float(sys.argv[3])
 gc_interval = int(sys.argv[4])
 seed = int(sys.argv[5])
-simplifier,atracker,tsim = ea.evolve_track_wrapper(popsize=N, rho=rho, seed=seed, gc_interval=gc_interval, mu=theta/float(4*N))
+simplifier,tsim = ea.evolve_track_wrapper(popsize=N, rho=rho, seed=seed, gc_interval=gc_interval, mu=theta/float(4*N))
 
 print(tsim,simplifier.times)
 np.random.seed(seed)

commit 151acd8cddf098ee737395edca513c91c601c8b4

fixed bug introduced in commit 309f1096c7aa27677483142687ed402f7b1b17a4 when not initializing tree need to set next_index to 2N after initializing tracker with 2N nodes minor: additional check in argsimplifier initializer when adding default metadata that there are mutations (generally not a problem before if there weren't, simply created an empty list, but might as well not create anything if uneeded)

fwdpy11_arg_example/ancestry_tracker.hpp
@@ -91,15 +91,16 @@ struct ancestry_tracker
         temp.reserve(N);
         //no need to reserve mutation space
 
-        //Initialize 2N nodes for the generation 0
         //if next_index == 0, then did not initialize with tree sequence
-        if (next_index_ == 0)
+        //so emplace back 2N nodes for the generation 0 and set next_index to 2N
+        if (next_index == 0)
             {
                 for (integer_type i = 0; i < 2 * N; ++i)
                     {
                         //ID, time 0, population 0
                         nodes.emplace_back(make_node(i, 0.0, 0));
                     }
+                next_index = 2 * N;
             }
     }
 

fwdpy11_arg_example/argsimplifier.py
@@ -30,7 +30,7 @@ class ArgSimplifier(object):
         if trees is not None:
             self.__process = False
             trees.dump_tables(nodes=self.__nodes, edges=self.__edges, sites = self.__sites, mutations = self.__mutations)
-            if(len(self.__mutations.metadata) == 0):
+            if(self.__mutations.num_rows > 0 and len(self.__mutations.metadata) == 0):
             	meta_list = [InitMeta(self.__sites[mut[0]][0], self.__nodes[mut[1]][1], "initial tree") for mut in self.__mutations]
             	encoded, offset = msprime.pack_bytes(list(map(pickle.dumps, meta_list)))
             	self.mutations.set_columns(site=self.__mutations.site, node=self.__mutations.node, derived_state=self.__mutations.derived_state, derived_state_offset=self.__mutations.derived_state_offset, parent=self.__mutations.parent, metadata_offset=offset, metadata=encoded)