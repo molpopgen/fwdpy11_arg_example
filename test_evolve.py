0f4d79c6539d1264a9e0b17cf38efbe0dbc7e7a0
David Lawrie <>
2018-04-01 22:13:44 -0700

    create and draw trees, check for mutation duplicates

--- test_evolve.py
+++ test_evolve.py
@@ -2,6 +2,7 @@ import fwdpy11_arg_example.evolve_arg as ea
 import msprime
 import numpy as np
 import sys
+import pickle
 
 N = int(sys.argv[1])
 rho = float(sys.argv[2])
@@ -17,9 +18,27 @@ np.random.seed(seed+1)
 curr_samples = np.random.choice(2*N, 10, replace = False).tolist()
 samples = curr_samples+evolver.anc_samples
 msprime.simplify_tables(samples, nodes = evolver.nodes, edges = evolver.edges, sites = evolver.sites, mutations = evolver.mutations)
-msp_rng = msprime.RandomGenerator(seed)
-sites = msprime.SiteTable()
-mutations = msprime.MutationTable()
+
+count = 0
+for idx, pos in enumerate(evolver.sites.position):
+    if(idx > 0 and pos == evolver.sites.position[idx-1]):
+      for idx_mut in evolver.mutations.site:
+          if(idx_mut == idx or idx_mut == idx-1):
+             print(idx_mut,pos,evolver.mutations[idx_mut].node,pickle.loads(evolver.mutations[idx_mut].metadata))
+      count += 1
+
+print(count)
+
+msp_rng = msprime.RandomGenerator(seed+2)
+neutral_sites = msprime.SiteTable()
+neutral_mutations = msprime.MutationTable()
 mutgen = msprime.MutationGenerator(msp_rng, theta/float(4*N)) # rho = theta
-mutgen.generate(evolver.nodes, evolver.edges, sites, mutations)
-print(sites.num_rows)
+mutgen.generate(evolver.nodes, evolver.edges, neutral_sites, neutral_mutations)
+
+
+trees_selected = msprime.load_tables(nodes=evolver.nodes, edges=evolver.edges, sites=evolver.sites, mutations=evolver.mutations)
+trees_neutral = msprime.load_tables(nodes=evolver.nodes, edges=evolver.edges, sites=neutral_sites, mutations=neutral_mutations)
+
+trees_selected.first().draw(path="/Users/dlawrie/tree_selected.svg", width=1500, height=1000, format="svg")
+trees_neutral.first().draw(path="/Users/dlawrie/tree_neutral.svg", width=1500, height=1000, format="svg")
+
