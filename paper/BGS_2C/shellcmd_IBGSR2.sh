7fe30e63da1bf86ab28ce2915d5e91816732a2c7
David Lawrie <>
2019-04-13 18:40:38 -0700

    updated seed values for shellcmd files and added R values to test

---
+++ paper/BGS_2C/shellcmd_IBGSR2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#$ -q krt2
+#$ -pe openmp 41
+
+cd $SGE_O_WORKDIR 
+
+module load krthornt/anaconda
+source activate fwdpy11_0_2_0
+
+PYTHONPATH=/share/kevin2/dlawrie/fwdpy11_arg_example python -i ../../test_evolve.py -o fwdpy11_sim2C_IBGS_R2.txt -g 2000 -1 flat 10000 -2 500 1 1001 -nT 1000 -R 2000 -m 0 0 0.05 3 3 0 -T 0 -B 200000 -niT -T 250 -s=-0.01 -ns1 100 --anc_sam1 1000 5 --anc_sam2 1000 5 -nc $CORES -S 30 -r 40 > nohup_IBGS_R2.txt
