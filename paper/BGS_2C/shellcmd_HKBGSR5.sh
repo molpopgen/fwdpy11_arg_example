f8263419d7d1d8f2089309860319537d6ae55a7d
David Lawrie <>
2019-04-14 01:48:38 -0700

    updated shell commands for HK95 model
    adds more cores, number replicates determined by using R11 as base r = (std/0.024375)^2
    std is the standard deviation of the experiment under 80 replicates
    (if the std of a lower R was lower than a higher R, the higher R's std was used)
    the constant 0.02... was calculated as the standard error for R11 0.39/sqrt(256)
    maximum number of replicates was 1000
    this somewhat normalizes the standard error across the HK models
    will be ~4x higher standard error compared to interleaved model

--- paper/BGS_2C/shellcmd_HKBGSR5.sh
+++ paper/BGS_2C/shellcmd_HKBGSR5.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #$ -q krt2
-#$ -pe openmp 81
+#$ -pe openmp 128
 
 cd $SGE_O_WORKDIR 
 
 module load krthornt/anaconda
 source activate fwdpy11_0_2_0
 
-PYTHONPATH=/share/kevin2/dlawrie/fwdpy11_arg_example python ../../test_evolve.py -o fwdpy11_sim2C_HKBGS_R5.txt -g 2000 -1 flat 10000 -2 500 1 1001 -nT 1000 -R 5000 -m 0 0 0.05 3 3 0 -T 0 -B 200000 -niT -T 250 -s=-0.01 -ns1 100 -95L --anc_sam1 1000 5 --anc_sam2 1000 5 -nc $CORES -S 23 -r 80 > nohup_HKBGS_R5.txt
+PYTHONPATH=/share/kevin2/dlawrie/fwdpy11_arg_example python ../../test_evolve.py -o fwdpy11_sim2C_HKBGS_R5.txt -g 2000 -1 flat 10000 -2 500 1 1001 -nT 1000 -R 5000 -m 0 0 0.05 3 3 0 -T 0 -B 200000 -niT -T 250 -s=-0.01 -ns1 100 -95L --anc_sam1 1000 5 --anc_sam2 1000 5 -nc $CORES -S 23 -r 1000 > nohup_HKBGS_R5.txt
