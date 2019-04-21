2a953b2b4948fb6dbabff0feba4770c66647e57a
David Lawrie <>
2019-04-20 23:14:33 -0700

    fixes to command line and shell commands (recombination rate and misc)

--- paper/BGS_1/shellcmd_HKBGSR5.sh
+++ paper/BGS_1/shellcmd_HKBGSR5.sh
@@ -8,4 +8,4 @@ cd $SGE_O_WORKDIR
 module load krthornt/anaconda
 source activate fwdpy11_0_2_0
 
-PYTHONPATH=/share/kevin2/dlawrie/fwdpy11_arg_example python ../../test_evolve.py -o fwdpy11_sim1_HKBGS_R5.txt -1 flat 3000 -2 3000 1 501 -nT 1000 -R 5000 -m 0 0 0.05 3 3 0 -B 60000 -niT -T 75 -s=-0.01 -ns1 100 -95L --anc_sam1 500 10 --anc_sam2 500 10 -nc $CORES -S 125 -r 1000 > nohup_HKBGS_R5.txt
+PYTHONPATH=/share/kevin2/dlawrie/fwdpy11_arg_example python ../../test_evolve.py -o fwdpy11_sim1_HKBGS_R5.txt -1 flat 3000 -2 3000 1 501 -nT 1000 -R 1500 -m 0 0 0.05 3 3 0 -B 60000 -niT -T 75 -s=-0.01 -ns1 100 -95L --anc_sam1 500 10 --anc_sam2 500 10 -nc $CORES -S 125 -r 1000 > nohup_HKBGS_R5.txt
