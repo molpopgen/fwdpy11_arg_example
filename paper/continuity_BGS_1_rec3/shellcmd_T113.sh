e7d0dc12e397cfe3788a665f442b27544c9ee485
David Lawrie <>
2019-07-12 06:43:22 -0700

    continuity BGS with duplication demography 1

---
+++ paper/continuity_BGS_1_rec3/shellcmd_T113.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#$ -q krt2
+#$ -pe openmp 101
+
+cd $SGE_O_WORKDIR 
+
+module load krthornt/anaconda
+source activate fwdpy11_0_2_0
+
+PYTHONPATH=/share/kevin2/dlawrie/fwdpy11_arg_example python ../../test_evolve.py -o fwdpy11_sim2C_IBGS_T113.txt -1 flat 3000 -2 3000 1 501 -nT 1000 -R 3300 -m 0 0 1 3 3 0 -B 60000 -niT -T 113 -s=-0.01 -ns1 100 --anc_sam1 500 10 --anc_sam2 500 10 -nc $CORES -S 330 -r 100 > nohup_IBGS_T113.txt
