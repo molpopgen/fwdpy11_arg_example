c42ccadfffbc7e674d019f2f1cb3e73af274f0e9
David Lawrie <>
2019-07-12 07:35:43 -0700

    corrected sim2C to sim1

--- paper/continuity_BGS_1_rec3/shellcmd_T113.sh
+++ paper/continuity_BGS_1_rec3/shellcmd_T113.sh
@@ -8,4 +8,4 @@ cd $SGE_O_WORKDIR
 module load krthornt/anaconda
 source activate fwdpy11_0_2_0
 
-PYTHONPATH=/share/kevin2/dlawrie/fwdpy11_arg_example python ../../test_evolve.py -o fwdpy11_sim2C_IBGS_T113.txt -1 flat 3000 -2 3000 1 501 -nT 1000 -R 3300 -m 0 0 1 3 3 0 -B 60000 -niT -T 113 -s=-0.01 -ns1 100 --anc_sam1 500 10 --anc_sam2 500 10 -nc $CORES -S 330 -r 100 > nohup_IBGS_T113.txt
+PYTHONPATH=/share/kevin2/dlawrie/fwdpy11_arg_example python ../../test_evolve.py -o fwdpy11_sim1_IBGS_T113.txt -1 flat 3000 -2 3000 1 501 -nT 1000 -R 3300 -m 0 0 1 3 3 0 -B 60000 -niT -T 113 -s=-0.01 -ns1 100 --anc_sam1 500 10 --anc_sam2 500 10 -nc $CORES -S 330 -r 100 > nohup_IBGS_T113.txt
