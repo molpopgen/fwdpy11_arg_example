04d95904d6175c61009d597aaa64ba8e8a1a4dc7
David Lawrie <>
2019-07-12 07:43:57 -0700

    forgot to add to previous commit

--- paper/continuity_BGS_2B_rec11/shellcmd_T0.sh
+++ paper/continuity_BGS_2B_rec11/shellcmd_T0.sh
@@ -8,4 +8,4 @@ cd $SGE_O_WORKDIR
 module load krthornt/anaconda
 source activate fwdpy11_0_2_0
 
-PYTHONPATH=/share/kevin2/dlawrie/fwdpy11_arg_example python ../../test_evolve.py -o fwdpy11_sim2C_neutral.txt -g 2000 -1 flat 10000 -2 500 1 1001 -nT 1000 -R 11000 -m 0 0 0.05 3 3 0 -T 0 -B 0 -T 0 -s=-0.01 -ns1 100 --anc_sam1 1000 5 --anc_sam2 1000 5 -nc $CORES -S 227 -r 10000 > nohup_IBGS_neutral.txt
+PYTHONPATH=/share/kevin2/dlawrie/fwdpy11_arg_example python ../../test_evolve.py -o fwdpy11_sim1_neutral.txt -g 2000 -1 flat 10000 -2 500 1 1001 -nT 1000 -R 11000 -m 0 0 0.05 3 3 0 -T 0 -B 0 -T 0 -s=-0.01 -ns1 100 --anc_sam1 1000 5 --anc_sam2 1000 5 -nc $CORES -S 227 -r 10000 > nohup_IBGS_neutral.txt
