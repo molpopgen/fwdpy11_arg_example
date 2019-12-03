0feb61b3f21142e04f09a3a92473139a1591618b
David Lawrie <>
2019-12-02 23:03:30 -0800

    increase replicates BGS_1_rec_3 by 16x for 1/4 se

--- paper/continuity_BGS_1_rec3/shellcmd_T90.sh
+++ paper/continuity_BGS_1_rec3/shellcmd_T90.sh
@@ -8,4 +8,4 @@ cd $SGE_O_WORKDIR
 module load krthornt/anaconda
 source activate fwdpy11_0_2_0
 
-PYTHONPATH=/share/kevin2/dlawrie/fwdpy11_arg_example python ../../test_evolve.py -o fwdpy11_sim1_IBGS_T90.txt -1 flat 3000 -2 3000 1 501 -nT 1000 -R 3300 -m 0 0 1 3 3 0 -B 60000 -niT -T 90 -s=-0.01 -ns1 100 --anc_sam1 500 10 --anc_sam2 500 10 -nc $CORES -S 329 -r 100 > nohup_IBGS_T90.txt
+PYTHONPATH=/share/kevin2/dlawrie/fwdpy11_arg_example python ../../test_evolve.py -o fwdpy11_sim1_IBGS_T90.txt -1 flat 3000 -2 3000 1 501 -nT 1000 -R 3300 -m 0 0 1 3 3 0 -B 60000 -niT -T 90 -s=-0.01 -ns1 100 --anc_sam1 500 10 --anc_sam2 500 10 -nc $CORES -S 329 -r 1600 > nohup_IBGS_T90.txt
